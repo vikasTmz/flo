7cdeb34c271dae75355875231be26ca9a1757ccc
vikasTmz <vikas_thamizharasan@brown.edu>
2022-11-22 17:33:27 -0500

    edited willmore_flow.pro

--- samples/host/willmore_flow/src/main_v2.cpp
+++ samples/host/willmore_flow/src/main_v2.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
-#include <numeric>
-#include <igl/write_triangle_mesh.h>
-#include <igl/read_triangle_mesh.h>
-#include "flo/host/flo_matrix_operation.hpp"
-#include "flo/host/willmore_flow.hpp"
-#include "flo/host/surface.hpp"
+// #include <numeric>
+// #include <igl/write_triangle_mesh.h>
+// #include <igl/read_triangle_mesh.h>
+// #include "flo/host/flo_matrix_operation.hpp"
+// #include "flo/host/willmore_flow.hpp"
+// #include "flo/host/surface.hpp"
 
 using namespace Eigen;
 
-template <typename T>
-struct ForwardEuler
-{
-  T tao = 0.95f;
+// template <typename T>
+// struct ForwardEuler
+// {
+//   T tao = 0.95f;
 
-  ForwardEuler(T i_tao) : tao(std::move(i_tao))
-  {
-  }
+//   ForwardEuler(T i_tao) : tao(std::move(i_tao))
+//   {
+//   }
 
-  void operator()(Eigen::Matrix<T, Eigen::Dynamic, 1>& i_x,
-                  const Eigen::Matrix<T, Eigen::Dynamic, 1>& i_dx) const
-  {
-    i_x += i_dx * tao;
-  }
-};
+//   void operator()(Eigen::Matrix<T, Eigen::Dynamic, 1>& i_x,
+//                   const Eigen::Matrix<T, Eigen::Dynamic, 1>& i_dx) const
+//   {
+//     i_x += i_dx * tao;
+//   }
+// };
 
 int main(int argc, char* argv[])
 {
