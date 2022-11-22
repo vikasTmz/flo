054ce1d7de0f7a9aa280e83137ff5fd950816663
vikasTmz <vikas_thamizharasan@brown.edu>
2022-11-22 18:41:58 -0500

    edited willmore_flow.pro: getting build to work

--- samples/host/willmore_flow/src/main_v2.cpp
+++ samples/host/willmore_flow/src/main_v2.cpp
@@ -44,7 +44,7 @@ int main(int argc, char* argv[])
   Eigen::Matrix<int, Eigen::Dynamic, 3> vertices;
   Eigen::Matrix<int, Eigen::Dynamic, 3> faces;
 
-  igl::read_triangle_mesh(in_name, vertices, faces);
+  igl::read_triangle_mesh('obj',in_name, vertices, faces);
 
   // igl::read_triangle_mesh(in_name, surf.vertices, surf.faces);
 
