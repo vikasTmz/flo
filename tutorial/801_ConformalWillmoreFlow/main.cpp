4ec3763fc6d922db595013f4f0130f3ce73f6dd2
vikasTmz <vikas_thamizharasan@brown.edu>
2022-12-04 19:56:16 -0500

    fixing igl mesh simplification error

--- tutorial/801_ConformalWillmoreFlow/main.cpp
+++ tutorial/801_ConformalWillmoreFlow/main.cpp
@@ -559,7 +559,7 @@ int main(int argc, char* argv[])
       MatrixXd temp_V = V;
       MatrixXi temp_F = F;
 
-      igl::decimate(temp_V, temp_F,num_face / 2, temp_V, temp_F);
+      igl::decimate(temp_V, (int)temp_F,num_face / 2, temp_V, temp_F);
       V = temp_V;
       F = temp_F;
     }
